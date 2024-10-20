4cff98e2cabb6b4f148e9f84dcf2586ec96e9d03
sonja <signtieva@gmail.com>
2024-10-20 11:59:21 +0300

    load local storage

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -5,23 +5,40 @@ const formData = {
     email: "",
     message: ""
 }
+
 const localStorageKey = "feedback-form-state";
 
+window.addEventListener('load', loadFormData);
+
+function loadFormData() {
+    const savedData = localStorage.getItem(localStorageKey);
+    if (savedData) {
+        const savedFormData = JSON.parse(savedData);
+        formData.email = savedFormData.email || '';
+        formData.message = savedFormData.message || '';
+
+        email.value = formData.email;
+        textarea.value = formData.message;
+    }
+}
+
+
 form.addEventListener('input', handleInput);
+
 function handleInput(event) {
-    const elements = event.currentTarget.elements;
-    formData.email = elements.email.value.trim();
-    formData.message = elements.message.value.trim();
+    formData.email = email.value.trim();
+    formData.message = textarea.value.trim();
 
     localStorage.setItem(localStorageKey, JSON.stringify(formData));
 };
 
 form.addEventListener('submit', handleSumbit);
+
 function handleSumbit(event) {
     event.preventDefault();
-    const elements = event.currentTarget.elements;
-    if (elements.email.value.trim() == '' || elements.message.value.trim() == '') {
-        alert `Fill please all fields`
+    if (email.value.trim() == '' || textarea.value.trim() == '') {
+        alert('Fill please all fields');
+        return;
     }
     console.log(JSON.parse(localStorage.getItem(localStorageKey)));
 
